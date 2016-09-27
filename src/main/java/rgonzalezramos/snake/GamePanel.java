0e5b5001914b316ebcad84fc9781454f68ae8e13
Roberto González Ramos <rgonzalezramos@gmail.com>
2016-09-28 00:18:57 +0200

    paint score

--- src/main/java/rgonzalezramos/snake/GamePanel.java
+++ src/main/java/rgonzalezramos/snake/GamePanel.java
@@ -13,18 +13,26 @@ public class GamePanel extends JPanel {
     }
 
     @Override
-    public void paint(Graphics g) {
+    public void paint(Graphics g_) {
+        super.paint(g_);
+
+        Graphics2D g = (Graphics2D) g_;
+
+        // TODO reuse this
         LocationConverter converter = LocationConverter.create(
                 game.getWidth(), game.getHeight(),
                 getWidth(), getHeight());
 
-        super.paint(g);
-
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        // Paint food
         GameState state = game.getState();
+
+        // Paint score
+        g.setColor(Color.GREEN);
+        g.drawString(String.format("Score: %4d", state.score), 20, 20);
+
+        // Paint food
         Rect rect = converter.rect(state.food);
         g.setColor(Color.RED);
         g.fillRect(rect.x, rect.y, rect.width, rect.height);
